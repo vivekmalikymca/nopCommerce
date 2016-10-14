b39aef03b82ade617398ceb6f4baae9ba73c6b95
skoshelev <sergey.k@nopcommerce.com>
2016-10-14 17:06:35 +0300

    Combine "Fixed Rate Shipping" and "Shipping by weight" plugins into single one

---
+++ src/Plugins/Nop.Plugin.Shipping.FixedOrByWeight/RouteProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Shipping.FixedOrByWeight
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.Shipping.FixedOrByWeight.SaveGeneralSettings",
+                 "Plugins/FixedOrByWeight/SaveGeneralSettings",
+                 new { controller = "FixedOrByWeight", action = "SaveGeneralSettings", },
+                 new[] { "Nop.Plugin.Shipping.FixedOrByWeight.Controllers" }
+            );
+
+            routes.MapRoute("Plugin.Shipping.FixedOrByWeight.AddPopup",
+                 "Plugins/FixedOrByWeight/AddPopup",
+                 new { controller = "FixedOrByWeight", action = "AddPopup" },
+                 new[] { "Nop.Plugin.Shipping.FixedOrByWeight.Controllers" }
+            );
+            routes.MapRoute("Plugin.Shipping.FixedOrByWeight.EditPopup",
+                 "Plugins/FixedOrByWeight/EditPopup",
+                 new { controller = "FixedOrByWeight", action = "EditPopup" },
+                 new[] { "Nop.Plugin.Shipping.FixedOrByWeight.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Shipping.FixedOrByWeight/ShippingByWeightSettings.cs
@@ -0,0 +1,12 @@
+﻿
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Shipping.FixedOrByWeight
+{
+    public class ShippingByWeightSettings : ISettings
+    {
+        public bool LimitMethodsToCreated { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
